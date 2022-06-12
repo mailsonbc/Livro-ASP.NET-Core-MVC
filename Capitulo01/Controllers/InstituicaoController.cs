e6192c6ebf3ccadb62f4e8e9b6289c8837e4239c
mailsonbc <mailsonbc@gmail.com>
2022-06-12 20:18:39 -0300

    Adicionando o projeto ao Git e ao Github

---
+++ Capitulo01/Controllers/InstituicaoController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Capitulo01.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Capitulo01.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        // Mock data
+        private static IList<Instituicao> instituicoes = new List<Instituicao>()
+        {
+            new Instituicao()
+            {
+                InstituicaoId = 1,
+                Nome = "UniParaná",
+                Endereco = "Paraná"
+            },
+            new Instituicao()
+            {
+                InstituicaoId = 2,
+                Nome = "UniSanta",
+                Endereco = "Santa Catarina"
+            },
+            new Instituicao()
+            {
+                InstituicaoId = 3,
+                Nome = "UniSãoPaulo",
+                Endereco = "São Paulo"
+            },
+            new Instituicao()
+            {
+                InstituicaoId = 4,
+                Nome = "UniSulgrandense",
+                Endereco = "Rio Grande do Sul"
+            },
+            new Instituicao()
+            {
+                InstituicaoId = 5,
+                Nome = "UniCarioca",
+                Endereco = "Rio de Janeiro"
+            }
+        };
+        //	Definição	de	uma	action	chamada	Index
+        public IActionResult Index()
+        {
+            return View(instituicoes.OrderBy(i => i.InstituicaoId));
+        }
+
+        //GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Instituicao instituicao)
+        {
+            instituicoes.Add(instituicao);
+            instituicao.InstituicaoId = instituicoes.Select(i => i.InstituicaoId).Max() + 1;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoId == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Instituicao instituicao)
+        {
+            instituicoes.Remove(instituicoes.Where(i => i.InstituicaoId == instituicao.InstituicaoId).First());
+            instituicoes.Add(instituicao);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoId == id).First());
+        }
+
+        public ActionResult Delete(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoId == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Instituicao instituicao)
+        {
+            instituicoes.Remove(instituicoes.Where(i => i.InstituicaoId == instituicao.InstituicaoId).First());
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Capitulo01/Data/IESContext.cs
@@ -0,0 +1,27 @@
+﻿using Capitulo01.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Capitulo01.Data
+{
+    public class IESContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Instituicao> Instituicoes { get; set; }
+
+        public IESContext(DbContextOptions<IESContext> options) : base(options)
+        {
+              
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Departamento>().ToTable("Departamento");
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database = IESCasaDoCodigo; Trusted_Connection = True; MultipleActiveResultSets = true");
+        //}
+    }
+}
